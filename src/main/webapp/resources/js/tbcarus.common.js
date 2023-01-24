0dc4f30410002c0533f17066a3071d5b1d679dac
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-01-24 22:00:38 +0300

    Notification

--- src/main/webapp/resources/js/tbcarus.common.js
+++ src/main/webapp/resources/js/tbcarus.common.js
@@ -3,6 +3,13 @@ let form;
 function makeEditable(datatableApi) {
     ctx.datatableApi = datatableApi;
     form = $('#detailsForm');
+
+    $(document).ajaxError(function (event, jqXHR, options, jsExc) {
+        failNoty(jqXHR);
+    });
+
+    // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
+    $.ajaxSetup({cache: false});
 }
 
 function add() {
@@ -33,6 +40,7 @@ function deleteRow(id) {
             type: "DELETE"
         }).done(function () {
             ctx.updateTable();
+            successNoty("Deleted");
         });
     }
 }
@@ -49,5 +57,35 @@ function save() {
     }).done(function () {
         $("#editRow").modal("hide");
         ctx.updateTable();
+        successNoty("Saved");
     });
+}
+
+let failedNote;
+
+function closeNoty() {
+    if (failedNote) {
+        failedNote.close();
+        failedNote = undefined;
+    }
+}
+
+function successNoty(text) {
+    closeNoty();
+    new Noty({
+        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
+        type: 'success',
+        layout: "bottomRight",
+        timeout: 2000
+    }).show();
+}
+
+function failNoty(jqXHR) {
+    closeNoty();
+    failedNote = new Noty({
+        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
+        type: "error",
+        layout: "bottomRight"
+    });
+    failedNote.show()
 }
