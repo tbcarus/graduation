7dddeeb343b4c996548fb3118a61e30068004fb9
tbcarus <70680359+tbcarus@users.noreply.github.com>
2023-03-07 22:30:36 +0300

    secure tag annotation

--- src/main/java/ru/tbcarus/topjava/web/RootController.java
+++ src/main/java/ru/tbcarus/topjava/web/RootController.java
@@ -3,6 +3,7 @@ package ru.tbcarus.topjava.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,6 +39,8 @@ public class RootController {
         return "redirect:users";
     }
 
+    //    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     public String getUsers() {
         log.info("users");
